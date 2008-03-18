76a3ce3fedab52b3e83d1113c0a91c234fba1c94
Reto Schüttel <reto@schuettel.ch>
2008-03-18 14:41:43 +0100

    new log combiner

---
+++ src/ch/hsr/ifs/pystructure/typeinference/inferencer/logger/CombinedLogger.java
@@ -0,0 +1,38 @@
+package ch.hsr.ifs.pystructure.typeinference.inferencer.logger;
+
+import ch.hsr.ifs.pystructure.typeinference.evaluators.base.GoalEvaluator;
+import ch.hsr.ifs.pystructure.typeinference.goals.base.IGoal;
+
+public class CombinedLogger implements IGoalEngineLogger {
+	private final IGoalEngineLogger[] loggers;
+
+	public CombinedLogger(IGoalEngineLogger... loggers) {
+		this.loggers = loggers;
+	}
+	
+
+	public void evaluationFinished(IGoal rootGoal) {
+		for (IGoalEngineLogger logger : loggers) {
+			logger.evaluationFinished(rootGoal);
+		}
+	}
+
+	public void evaluationStarted(IGoal rootGoal) {
+		for (IGoalEngineLogger logger : loggers) {
+			logger.evaluationStarted(rootGoal);
+		}
+	}
+
+	public void goalCreated(IGoal goal, GoalEvaluator creator, GoalEvaluator evaluator) {
+		for (IGoalEngineLogger logger : loggers) {
+			logger.goalCreated(goal, creator, evaluator);
+		}
+	}
+
+	public void goalFinished(IGoal goal, GoalEvaluator evaluator) {
+		for (IGoalEngineLogger logger : loggers) {
+			logger.goalFinished(goal, evaluator);
+		}
+	}
+
+}
