ffa527e361f763586914adb41eae90f16675d8e6
Reto Schüttel <reto@schuettel.ch>
2008-03-19 19:07:23 +0100

    oups, I forgot it again

---
+++ src/ch/hsr/ifs/pystructure/typeinference/goals/references/CallableGoal.java
@@ -0,0 +1,18 @@
+package ch.hsr.ifs.pystructure.typeinference.goals.references;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.hsr.ifs.pystructure.typeinference.contexts.ModuleContext;
+import ch.hsr.ifs.pystructure.typeinference.goals.base.AbstractGoal;
+import ch.hsr.ifs.pystructure.typeinference.results.references.FunctionReference;
+
+public abstract class CallableGoal extends AbstractGoal {
+	public final List<FunctionReference> references;
+
+	public CallableGoal(ModuleContext context) {
+		super(context);
+		references = new ArrayList<FunctionReference>();
+	}
+
+}
