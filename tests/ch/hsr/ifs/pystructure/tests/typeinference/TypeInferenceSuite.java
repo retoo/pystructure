156d4dba886ff480edcd54374decd9508272473a
Reto Schüttel <reto@schuettel.ch>
2008-03-20 17:29:09 +0100

    The TypeinferencerSuite now also runs autonomously

--- tests/ch/hsr/ifs/pystructure/tests/typeinference/TypeInferenceSuite.java
+++ tests/ch/hsr/ifs/pystructure/tests/typeinference/TypeInferenceSuite.java
@@ -14,6 +14,7 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.LinkedList;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestResult;
 import junit.framework.TestSuite;
@@ -21,12 +22,17 @@ import ch.hsr.ifs.pystructure.playground.Structure101Logger;
 import ch.hsr.ifs.pystructure.typeinference.inferencer.PythonTypeInferencer;
 import ch.hsr.ifs.pystructure.typeinference.inferencer.logger.CombinedLogger;
 import ch.hsr.ifs.pystructure.typeinference.inferencer.logger.IGoalEngineLogger;
+import ch.hsr.ifs.pystructure.typeinference.inferencer.logger.StatsLogger;
 import ch.hsr.ifs.pystructure.typeinference.visitors.Workspace;
 
 public class TypeInferenceSuite extends TestSuite {
 	
 	private PythonTypeInferencer inferencer;
 
+	public static Test suite() {
+		return new TypeInferenceSuite("tests/python/typeinference/");
+	}
+	
 	public TypeInferenceSuite(String testsDirectory) {
 		super(testsDirectory);
 		File dir = new File(testsDirectory);
