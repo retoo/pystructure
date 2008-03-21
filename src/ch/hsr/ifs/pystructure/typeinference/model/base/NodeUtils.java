b56bfc8f5a33bad82d63e0554bbbcf952a7f97b0
Reto Schüttel <reto@schuettel.ch>
2008-03-21 11:07:43 +0100

    new SimpleNode pretty printer

--- src/ch/hsr/ifs/pystructure/typeinference/model/base/NodeUtils.java
+++ src/ch/hsr/ifs/pystructure/typeinference/model/base/NodeUtils.java
@@ -9,9 +9,71 @@ public final class NodeUtils {
 
 	public static String nodePosition(SimpleNode node) {
 		return 
-			node != null
-				? "(L" + node.beginLine + " C" + node.beginColumn + ")"
+		node != null
+		? "(L" + node.beginLine + " C" + node.beginColumn + ")"
 				: "";
 	}
-	
+
+
+	/**
+	 * Pretty-print the given AST node to stdout.
+	 *
+	 * @param node
+	 */
+	public static void prettyPrint(SimpleNode node) {
+		System.out.println(getPrettyPrinted(node));
+	}
+
+	/**
+	 * Returns a pretty-printed given ast
+	 *
+	 * @param node top-level node
+	 * @return string containing the human-readable ast
+	 */
+	public static String getPrettyPrinted(SimpleNode node) {
+		String str = node.toString();
+
+		StringBuilder out = new StringBuilder();
+
+		int level = 0;
+		boolean skipSpace = false;
+
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+
+			if (c == '[') {
+				out.append("");
+				out.append("\n");
+				level++;
+				printIdent(out, level);
+			} else if (c == ',') {
+				out.append(c);
+				out.append("\n");
+				printIdent(out, level);
+				skipSpace = true;
+
+			} else if (c == ' ' && skipSpace) {
+				skipSpace = false;
+			} else if (c == ']') {
+				level--;
+				out.append("\n");
+				printIdent(out, level);
+				//out.append("]");
+			} else if (c == '=') {
+				out.append(" = ");
+			} else {
+				out.append(c);
+			}
+		}
+
+
+		return out.toString();
+	}
+
+	private static void printIdent(StringBuilder out, int level) {
+		for (int i = 0; i < level; i++) {
+			out.append("|   ");
+		}
+	}
+
 }
