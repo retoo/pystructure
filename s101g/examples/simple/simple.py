41aee0f77516ef7ead295786efa2028a9a2fcad7
Reto Schüttel <reto@schuettel.ch>
2008-03-25 13:33:09 +0100

    New simple example

--- s101g/examples/simple/simple.py
+++ s101g/examples/simple/simple.py
@@ -1,17 +1,13 @@
-class Attributes(object):
-    def __init__(self, arg):
-        self.attr_a = arg
-        self.attr_b = 1
-        
-    def update(self):
-        self.attr_b = 2
 
-
-a = Attributes()
-a.update
-
-a.attr_b
-a.attr_b
-a.attr_a
-a.attr_b
-
+class Foo(object):
+  def set(self, value):
+    self.field = value
+            
+  def get(self):
+    return self.field
+                    
+a = Foo()
+a.set("hello world")
+z = a.get()
+print z
+z
