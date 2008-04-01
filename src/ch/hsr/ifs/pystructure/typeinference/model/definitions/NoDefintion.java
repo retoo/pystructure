6d5b4c02ee67e910e05eff36b4ad933161e1fcda
Reto Schüttel <reto@schuettel.ch>
2008-04-01 22:48:21 +0200

    And here comes the nodefinition

---
+++ src/ch/hsr/ifs/pystructure/typeinference/model/definitions/NoDefintion.java
@@ -0,0 +1,10 @@
+package ch.hsr.ifs.pystructure.typeinference.model.definitions;
+
+public class NoDefintion extends Definition {
+
+	public NoDefintion(Module module, String name) {
+		super(module, name, null);
+		
+	}
+	
+}
