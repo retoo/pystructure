fa7f7d753840e124793e14f062445f2fbae65252
Robin Stocker <robin@nibor.org>
2008-04-01 23:13:03 +0200

    Eclipse, your code style does not conform to the style guide. Out!!!1!!

--- src/ch/hsr/ifs/pystructure/typeinference/model/definitions/Attribute.java
+++ src/ch/hsr/ifs/pystructure/typeinference/model/definitions/Attribute.java
@@ -42,23 +42,30 @@ public class Attribute extends Definition {
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj)
+		if (this == obj) {
 			return true;
-		if (!super.equals(obj))
+		}
+		if (!super.equals(obj)) {
 			return false;
-		if (getClass() != obj.getClass())
+		}
+		if (getClass() != obj.getClass()) {
 			return false;
+		}
 		final Attribute other = (Attribute) obj;
 		if (klass == null) {
-			if (other.klass != null)
+			if (other.klass != null) {
 				return false;
-		} else if (!klass.equals(other.klass))
+			}
+		} else if (!klass.equals(other.klass)) {
 			return false;
+		}
 		if (name == null) {
-			if (other.name != null)
+			if (other.name != null) {
 				return false;
-		} else if (!name.equals(other.name))
+			}
+		} else if (!name.equals(other.name)) {
 			return false;
+		}
 		return true;
 	}
 	
