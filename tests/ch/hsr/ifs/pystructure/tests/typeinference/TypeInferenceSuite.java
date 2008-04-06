b9c1078dfdc9e13b9b18e38e88827e9ef114a632
Robin Stocker <robin@nibor.org>
2008-04-06 23:37:21 +0200

    Shuffle test files using a seed so that a test failure caused by the run order can be reproduced. Before this, the run order depended on the file system.

--- tests/ch/hsr/ifs/pystructure/tests/typeinference/TypeInferenceSuite.java
+++ tests/ch/hsr/ifs/pystructure/tests/typeinference/TypeInferenceSuite.java
@@ -12,6 +12,10 @@ package ch.hsr.ifs.pystructure.tests.typeinference;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -24,23 +28,31 @@ import ch.hsr.ifs.pystructure.typeinference.inferencer.logger.IGoalEngineLogger;
 import ch.hsr.ifs.pystructure.typeinference.visitors.Workspace;
 
 public class TypeInferenceSuite extends TestSuite {
-	
+
 	private PythonTypeInferencer inferencer;
+
+	private class PythonFilenameFilter implements FilenameFilter {
+		public boolean accept(File file, String name) {
+			return name.endsWith(".py");
+		}
+	}
 
 	public static Test suite() {
 		return new TypeInferenceSuite("tests/python/typeinference/");
 	}
-	
+
 	public TypeInferenceSuite(String testsDirectory) {
 		super(testsDirectory);
 		File dir = new File(testsDirectory);
+		List<File> files = Arrays.asList(dir.listFiles(new PythonFilenameFilter()));
 		
-		File[] files = dir.listFiles(new FilenameFilter() {
-			public boolean accept(File file, String name) {
-				return name.matches(".+\\.py$");
-			}
-		});
-		
+		/*
+		 * Shuffle test files using a seed so that a test failure caused by the
+		 * run order can be reproduced.
+		 */
+		long seed = System.currentTimeMillis();
+		System.out.println("Seed used for shuffling test files: " + seed);
+		Collections.shuffle(files, new Random(seed));
 		
 		Structure101Logger s101log = new Structure101Logger();
 		IGoalEngineLogger logger = new CombinedLogger(s101log);
