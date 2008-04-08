34799e6d65a012585911004643d83f4cc1baf0ac
Reto Schüttel <reto@schuettel.ch>
2008-04-08 10:33:40 +0200

    new IGoal (2/2)

--- src/ch/hsr/ifs/pystructure/typeinference/goals/base/IGoal.java
+++ src/ch/hsr/ifs/pystructure/typeinference/goals/base/IGoal.java
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
-
- *******************************************************************************/
 package ch.hsr.ifs.pystructure.typeinference.goals.base;
 
 import java.util.ArrayList;
@@ -27,5 +18,5 @@ public interface IGoal {
 	 *         none.
 	 */
 	ModuleContext getContext();
-
+	
 }
