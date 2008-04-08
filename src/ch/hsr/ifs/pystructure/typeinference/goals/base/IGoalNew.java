5594a8224da9ed2367239573512f60cc390e9714
Reto Schüttel <reto@schuettel.ch>
2008-04-08 10:33:14 +0200

    new IGoal (1/2)

---
+++ src/ch/hsr/ifs/pystructure/typeinference/goals/base/IGoalNew.java
@@ -0,0 +1,22 @@
+package ch.hsr.ifs.pystructure.typeinference.goals.base;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.hsr.ifs.pystructure.typeinference.contexts.ModuleContext;
+
+public interface IGoalNew {
+
+	List<IGoal> NO_GOALS = new ArrayList<IGoal>();
+
+	/**
+	 * Returns context, in which this goal should be considered. Context
+	 * contains, for example, the instance of the class a method is called of,
+	 * precalculated scope or something like that.
+	 *
+	 * @return The context of this goal, or <code>null</code> is there is
+	 *         none.
+	 */
+	ModuleContext getContext();
+	
+}
