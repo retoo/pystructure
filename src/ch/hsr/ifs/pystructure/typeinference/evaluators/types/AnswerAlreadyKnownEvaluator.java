1e42b739ed69fe82b6dec37e26947c0daa88d5a8
Reto Schüttel <reto@schuettel.ch>
2008-04-08 11:21:17 +0200

    Oups, forgot a file :)

---
+++ src/ch/hsr/ifs/pystructure/typeinference/evaluators/types/AnswerAlreadyKnownEvaluator.java
@@ -0,0 +1,27 @@
+package ch.hsr.ifs.pystructure.typeinference.evaluators.types;
+
+import java.util.List;
+
+import ch.hsr.ifs.pystructure.typeinference.basetype.IType;
+import ch.hsr.ifs.pystructure.typeinference.evaluators.base.AbstractEvaluator;
+import ch.hsr.ifs.pystructure.typeinference.goals.base.GoalState;
+import ch.hsr.ifs.pystructure.typeinference.goals.base.IGoal;
+import ch.hsr.ifs.pystructure.typeinference.goals.types.AbstractTypeGoal;
+
+public class AnswerAlreadyKnownEvaluator extends AbstractEvaluator {
+
+	public AnswerAlreadyKnownEvaluator(AbstractTypeGoal goal, IType classType) {
+		super(goal);
+		
+		goal.resultType.appendType(classType);
+	}
+
+	public List<IGoal> init() {
+		return IGoal.NO_GOALS;
+	}
+
+	public List<IGoal> subgoalDone(IGoal subgoal, GoalState state) {
+		return IGoal.NO_GOALS;
+	}
+	
+}
