26029aaea6df94d0b56c2601415e991673f99e35
Reto Schüttel <reto@schuettel.ch>
2008-04-09 11:49:47 +0200

    New ClassType

---
+++ src/ch/hsr/ifs/pystructure/typeinference/results/types/ClassType.java
@@ -0,0 +1,29 @@
+package ch.hsr.ifs.pystructure.typeinference.results.types;
+
+import ch.hsr.ifs.pystructure.typeinference.model.definitions.Class;
+import ch.hsr.ifs.pystructure.typeinference.model.definitions.Module;
+
+public class ClassType extends AbstractType {
+	private Module module;
+	private Class klass;
+	
+	public ClassType(String typeName) {
+		super(typeName);
+		/* FIXME: shouldn't we use a different class for that? */
+	}
+
+	public ClassType(Module module, Class klass) {
+		super(klass.getName());
+		this.module = module;
+		this.klass = klass;
+	}
+	
+	public Module getModule() {
+		return module;
+	}
+	
+	public Class getKlass() {
+		return klass;
+	}
+
+}
