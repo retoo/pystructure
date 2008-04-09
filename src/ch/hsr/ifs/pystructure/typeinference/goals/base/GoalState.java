8dfa74351b65cf72ae8764e0d7b56465b3c9ac75
Robin Stocker <robin@nibor.org>
2008-04-09 11:03:25 +0200

    Removed copyright because it is no longer applicable (it wasn't an enum before, values changed, etc.)

--- src/ch/hsr/ifs/pystructure/typeinference/goals/base/GoalState.java
+++ src/ch/hsr/ifs/pystructure/typeinference/goals/base/GoalState.java
@@ -1,15 +1,5 @@
-/*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
-
- *******************************************************************************/
-
 package ch.hsr.ifs.pystructure.typeinference.goals.base;
 
 public enum GoalState {
-	DONE, WAITING, PRUNED, RECURSIVE
+	DONE, RECURSIVE
 }
