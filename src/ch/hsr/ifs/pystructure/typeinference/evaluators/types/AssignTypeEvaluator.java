4348b5518582c1e9b3d5daa829345f7ac2397e60
Robin Stocker <robin@nibor.org>
2008-04-15 15:38:25 +0200

    Small refactoring.

--- src/ch/hsr/ifs/pystructure/typeinference/evaluators/types/AssignTypeEvaluator.java
+++ src/ch/hsr/ifs/pystructure/typeinference/evaluators/types/AssignTypeEvaluator.java
@@ -24,6 +24,7 @@ package ch.hsr.ifs.pystructure.typeinference.evaluators.types;
 
 import java.util.List;
 
+import ch.hsr.ifs.pystructure.typeinference.contexts.ModuleContext;
 import ch.hsr.ifs.pystructure.typeinference.goals.base.GoalState;
 import ch.hsr.ifs.pystructure.typeinference.goals.base.IGoal;
 import ch.hsr.ifs.pystructure.typeinference.goals.types.AbstractTypeGoal;
@@ -49,11 +50,12 @@ public class AssignTypeEvaluator extends DefinitionTypeEvaluator  {
 
 	@Override
 	public List<IGoal> init() {
+		ModuleContext context = getGoal().getContext();
 		Value value = assignDefinition.getValue();
 		if (value instanceof TupleElement) {
-			return wrap(new TupleElementTypeGoal(getGoal().getContext(), (TupleElement) value));
+			return wrap(new TupleElementTypeGoal(context, (TupleElement) value));
 		} else {
-			return wrap(new ExpressionTypeGoal(getGoal().getContext(), assignDefinition.getValue().getExpression()));
+			return wrap(new ExpressionTypeGoal(context, value.getExpression()));
 		}
 	}
 
