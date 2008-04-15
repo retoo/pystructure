4c17a643562973ae37f9e46982327fef524d06fa
Reto Schüttel <reto@schuettel.ch>
2008-04-15 16:04:15 +0200

    new type annotator

--- src/ch/hsr/ifs/pystructure/playground/TypeAnnotator.java
+++ src/ch/hsr/ifs/pystructure/playground/TypeAnnotator.java
@@ -23,6 +23,8 @@
 package ch.hsr.ifs.pystructure.playground;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -31,6 +33,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.jdom.output.XMLOutputter;
 import org.python.pydev.parser.jython.SimpleNode;
 import org.python.pydev.parser.jython.ast.exprType;
 
@@ -43,10 +46,92 @@ import ch.hsr.ifs.pystructure.typeinference.model.definitions.StructureDefinitio
 import ch.hsr.ifs.pystructure.typeinference.visitors.Workspace;
 import ch.hsr.ifs.pystructure.utils.LineIterator;
 
-public final class TypeAnnotator {
+public class TypeAnnotator {
 	
-	private TypeAnnotator() {
+	private Workspace workspace;
+	private File outPath;
+	private XMLOutputter escaper;
+	private PythonTypeInferencer inferencer;
+	private Foo foo;
+	public TypeAnnotator(File path, String outPath) {
+		this.workspace = new Workspace(path);
+		this.outPath = new File(outPath);
+		this.escaper = new XMLOutputter();
+		this.inferencer = new PythonTypeInferencer(new StatsLogger(false));
 	}
+	
+	public void generateReport() throws IOException {
+		
+		for (Module module : workspace.getModules()) {
+			List<Result> results = new ArrayList<Result>();
+			
+			Spider spider = new Spider();
+			spider.run(module);
+			
+			for (Map.Entry<StructureDefinition, List<exprType>> entry : spider.getTypables().entrySet()) {
+				StructureDefinition definition = entry.getKey();
+				List<exprType> expressions = entry.getValue();
+				
+				for (exprType node : expressions) {
+					IType type = inferencer.evaluateType(workspace, module, node);
+					results.add(new Result(definition, node, type));
+				}
+			}
+			
+			String filename = module.getNamePath().toString() + ".html";
+			File outFile = new File(outPath, filename);
+			System.out.println(outFile);
+			
+			String out = escaper.escapeAttributeEntities(generateDebugSourceOutput(module, results));
+			
+			FileWriter writer = new FileWriter(outFile);
+			
+			writer.write("<pre>");
+			writer.write(out);
+			writer.write("</pre>");
+			
+			writer.close();
+		}
+		inferencer.shutdown();
+		
+		
+	}
+
+
+	private String generateDebugSourceOutput(Module module, List<Result> results) {
+		StringBuilder sb = new StringBuilder();
+		
+		/* group results by line nr */
+		HashMap<Integer, List<Result>> types = new HashMap<Integer, List<Result>>();
+		for (Result result : results) {
+			List<Result> l = types.get(result.node.beginLine);
+			if (l == null) {
+				l = new LinkedList<Result>();
+				types.put(result.node.beginLine, l);
+			}
+			l.add(result);
+		}
+		
+		
+		/* print out source */
+		int i = 1;
+		for (String line : new LineIterator(new StringReader(module.getSource()))) {
+			sb.append(line);
+			
+			List<Result> lineType = types.get(i);
+
+			if (lineType != null) {
+				Collections.sort(lineType);
+				sb.append(" # " + lineType + "\n");
+			} else {
+				sb.append("\n");
+			}
+			i++;
+		}
+		
+		return sb.toString();
+	}
+	
 	
 	private static final class Result implements Comparable<Result> {
 		public final StructureDefinition definition;
@@ -85,65 +170,10 @@ public final class TypeAnnotator {
 
 	public static void main(String[] args) throws Exception {
 		File path = new File("s101g/examples/pydoku/");
-		Workspace workspace = new Workspace(path);
 		
-		PythonTypeInferencer inferencer = new PythonTypeInferencer(new StatsLogger(false));
+		TypeAnnotator ta = new TypeAnnotator(path, "out");
 		
-		for (Module module : workspace.getModules()) {
-			
-			List<Result> results = new ArrayList<Result>();
-			
-			Spider spider = new Spider();
-			spider.run(module);
-			
-			for (Map.Entry<StructureDefinition, List<exprType>> entry : spider.getTypables().entrySet()) {
-				StructureDefinition definition = entry.getKey();
-				List<exprType> expressions = entry.getValue();
-				
-				for (exprType node : expressions) {
-					IType type = inferencer.evaluateType(workspace, module, node);
-					results.add(new Result(definition, node, type));
-				}
-			}
-			
-			
-			System.out.println(generateDebugSourceOutput(module, results));
-		}
-		inferencer.shutdown();
-	}
-
-	private static String generateDebugSourceOutput(Module module, List<Result> results) {
-		StringBuilder sb = new StringBuilder();
-		
-		/* group results by line nr */
-		HashMap<Integer, List<Result>> types = new HashMap<Integer, List<Result>>();
-		for (Result result : results) {
-			List<Result> l = types.get(result.node.beginLine);
-			if (l == null) {
-				l = new LinkedList<Result>();
-				types.put(result.node.beginLine, l);
-			}
-			l.add(result);
-		}
-		
-		
-		/* print out source */
-		int i = 1;
-		for (String line : new LineIterator(new StringReader(module.getSource()))) {
-			sb.append(line);
-			
-			List<Result> lineType = types.get(i);
-
-			if (lineType != null) {
-				Collections.sort(lineType);
-				sb.append(" # " + lineType + "\n");
-			} else {
-				sb.append("\n");
-			}
-			i++;
-		}
-		
-		return sb.toString();
+		ta.generateReport();
 	}
 
 }
