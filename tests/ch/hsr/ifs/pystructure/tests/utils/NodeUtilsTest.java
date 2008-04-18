b6d7e160e4165ac20d1550a3c0f51ed260af12df
Robin Stocker <robin@nibor.org>
2008-04-18 17:41:43 +0200

    Test for getPrettyPrinted.

--- tests/ch/hsr/ifs/pystructure/tests/utils/NodeUtilsTest.java
+++ tests/ch/hsr/ifs/pystructure/tests/utils/NodeUtilsTest.java
@@ -1,6 +1,7 @@
 package ch.hsr.ifs.pystructure.tests.utils;
 
 import org.python.pydev.parser.jython.ParseException;
+import org.python.pydev.parser.jython.ast.Assign;
 import org.python.pydev.parser.jython.ast.Attribute;
 import org.python.pydev.parser.jython.ast.Call;
 import org.python.pydev.parser.jython.ast.Expr;
@@ -26,6 +27,26 @@ public class NodeUtilsTest extends TestCase {
 		call.func.parent = call;
 		
 		assertEquals(call, NodeUtils.getCallForFunc(call.func));
+	}
+	
+	public void testGetPrettyPrinted() throws ParseException {
+		Module module = Parser.parse("x = 42");
+		Assign assign = (Assign) module.body[0];
+		String expected =
+				"Assign\n" +
+				"|   targets = \n" +
+				"|   |   Name\n" +
+				"|   |   |   id = x,\n" +
+				"|   |   |   ctx = Store\n" +
+				"|   |   \n" +
+				"|   ,\n" +
+				"|   value = Num\n" +
+				"|   |   n = 42,\n" +
+				"|   |   type = Int,\n" +
+				"|   |   num = 42\n" +
+				"|   \n" +
+				"";
+		assertEquals(expected, NodeUtils.getPrettyPrinted(assign));
 	}
 	
 }
