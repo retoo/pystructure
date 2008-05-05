bf026e857ef3c19f313cfd4dec83d5af2f4763b3
Robin Stocker <robin@nibor.org>
2008-05-05 13:46:53 +0200

    Implementation of some list methods.

--- resources/python-stdlib/builtins.py
+++ resources/python-stdlib/builtins.py
@@ -5,5 +5,26 @@ class list(object):
     def append(self, element):
         self.element = element
 
+    def count(self, element):
+        return 1
+
+    def extend(self, other):
+        self.element = other[0]
+
+    def index(self, element):
+        return 1
+
+    def insert(self, index, element):
+        self.element = element
+
     def pop(self):
         return self.element
+
+    def remove(self):
+        pass
+
+    def reverse(self):
+        pass
+
+    def sort(self):
+        pass
--- tests/python/typeinference/built-ins.py
+++ tests/python/typeinference/built-ins.py
@@ -5,3 +5,9 @@ a.pop() ## type int
 b = list()
 b.append(3.14)
 b.pop() ## type float
+
+c = list()
+c.append(42)
+c.insert(0, "test")
+c.pop() ## type int|str
+c.count() ## type int
