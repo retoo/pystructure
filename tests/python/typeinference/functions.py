8857edf408bc2d336a5beb21fe5fb8fb2274e5fb
Robin Stocker <robin@nibor.org>
2008-05-19 10:57:37 +0200

    Tests for finding references of a function which is used like this: module.function()

---
+++ tests/python/typeinference/functions.py
@@ -0,0 +1,5 @@
+def get_random_number(arg):
+    arg ## type float|int
+    return 4
+
+get_random_number(42) ## type int
---
+++ tests/python/typeinference/functions_user.py
@@ -0,0 +1,3 @@
+import functions
+
+functions.get_random_number(3.14) ## type int
