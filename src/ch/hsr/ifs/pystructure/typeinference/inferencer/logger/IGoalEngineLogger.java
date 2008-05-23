8fa1776087a8d30274de2a23aa2ba46dc896fb4a
Reto Schüttel <reto@schuettel.ch>
2008-05-23 15:47:55 +0200

    JavaDoc for Logger

--- src/ch/hsr/ifs/pystructure/typeinference/inferencer/logger/IGoalEngineLogger.java
+++ src/ch/hsr/ifs/pystructure/typeinference/inferencer/logger/IGoalEngineLogger.java
@@ -25,14 +25,50 @@ package ch.hsr.ifs.pystructure.typeinference.inferencer.logger;
 import ch.hsr.ifs.pystructure.typeinference.evaluators.base.AbstractEvaluator;
 import ch.hsr.ifs.pystructure.typeinference.goals.base.IGoal;
 
+/**
+ * Generic interface for loggers of the goal engine. The logger is being used
+ * during the whole lifetime of the type inferencer.
+ * 
+ * For a goal tree aware logger see {@link GoalTreeLogger}.
+ */
 public interface IGoalEngineLogger {
 
-	void evaluationStarted(IGoal rootGoal);
-	void evaluationFinished(IGoal rootGoal);
+	/**
+	 * Hook which gets called when a new evaluation has been started. This 
+	 * is the root goal (quite often an ExpressionTypeGoal) which then causes
+	 * all the subgoals to be created. 
+	 * 
+	 * @param goal the initial root goal submitted by the user of the library
+	 */
+	void evaluationStarted(IGoal goal);
+	
+	
+	/**
+	 * Hook which gets called when an evaluation has been finished. This 
+	 * 
+	 * @param goal the initial goal which is now solved
+	 */
+	void evaluationFinished(IGoal goal);
 
+	/**
+	 * Hook which gets called when a subgoal was been created by one of the evaluators.
+	 * 
+	 * @param goal The newly created goal 
+	 * @param creator The original evaluator which created that particular goal
+	 * @param evaluator The evaluator which is being used to process this goal
+	 */
 	void goalCreated(IGoal goal, AbstractEvaluator creator, AbstractEvaluator evaluator);
+	/**
+	 * Hook which gets called when a subgoal has been finished (successfully or not)
+	 * 
+	 * @param goal Goal which got finished (successfully or not)
+	 * @param evaluator Evaluator which was responsible for the processing of this goal
+	 */
 	void goalFinished(IGoal goal, AbstractEvaluator evaluator);
 
+	/**
+	 * Hook when the inferencer is about to shutdown.
+	 */
 	void shutdown();
 
 }
