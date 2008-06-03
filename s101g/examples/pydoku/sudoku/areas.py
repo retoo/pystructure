66c5f71c38a3dab27321dc9824107add3a6785d2
Robin Stocker <robin@nibor.org>
2008-06-03 16:54:50 +0200

    Some fixes for Pydoku.

--- s101g/examples/pydoku/sudoku/areas.py
+++ s101g/examples/pydoku/sudoku/areas.py
@@ -18,14 +18,14 @@
 class Area(object):
     def __init__(self, i):
         self.i = i
-        self.fields = []
+        self.area_fields = []
         self.available_numbers = set(range(1, 10))
         
     def append(self, field):
-        self.fields.append(field)
+        self.area_fields.append(field)
         
     def __iter__(self):
-        return self.fields.__iter__()
+        return self.area_fields.__iter__()
     
     def __str__(self):
         return str(self.i)
