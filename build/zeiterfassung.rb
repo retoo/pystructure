ba168ad536094056ab1c1130e654dd8609611cc2
Reto Schüttel <reto@schuettel.ch>
2008-06-04 18:12:48 +0200

    super duper time erfassung

--- build/zeiterfassung.rb
+++ build/zeiterfassung.rb
@@ -2,8 +2,12 @@
 
 require 'date'
 
-VALID_TIME = /^\d\d:\d\d$/
+VALID_TIME = /^\d?\d:\d\d$/
+VALID_EVENT = /^(-?\d+(?:\.\d+)?)h\s*(.*)$/
+VALID_SPAN = /^(\d?\d:\d\d\s*-\s*\d\d:\d\d)\s*(.*)$/
+
 TIME_FORMAT = /-/
+
 
 def convert_to_hours(time)
   # time is 12:34
@@ -14,6 +18,19 @@ def convert_to_hours(time)
   hour, minute = time.split(":").map{|s| s.to_i }
   
   return hour + (minute.to_f / 60)
+end
+
+def get_time_span(time) 
+  # 12:00-13:30
+  times = time.split(/\s*-\s*/);
+  
+  if times.length != 2
+    raise "Ungültiges Zeitformat #{record}"
+  end
+  
+  from, to = times.map{|t| convert_to_hours(t) }
+
+  return to - from
 end
 
 
@@ -29,46 +46,47 @@ STDIN.each do |line|
       break
     end
     
-    (blurb, date_s, hours_s, desc) = line.split("||").collect{|e| e.strip }
+    (week_s, weekday_s, times) = line.split("|").collect{|e| e.strip }
 
-    if date_s.match /Datum/
-      puts line
-      next
-    end
+    #date = Date.parse(date_s)
     
-    date = Date.parse(date_s)
+    week = week_s.to_i
     
-    # calculate week
-    days_since_start = date - project_start
-    week = (days_since_start / 7).ceil
+    today = 0.0
     
-    time = 0.0
-    
-    if hours_s.match TIME_FORMAT
-      records = hours_s.split(/,/)
-      
-      records.map{|r| r.strip }.each do |record| 
-        # 12:00-13:30
-        times = record.split(/\s*-\s*/);
-        
-        if times.length != 2
-          raise "Ungültiges Zeitformat #{record}"
-        end
-        
-        from, to = times.map{|t| convert_to_hours(t) }
-
-        time += (to - from)
+    comment, hours = nil, nil
+    times.split(",").collect{|x| x.strip}.each do |time| 
+      if event = VALID_EVENT.match(time)
+        comment = event[2]
+        hours = event[1].to_f
+      elsif span_match = VALID_SPAN.match(time)
+        span = span_match[1]
+        comment = span_match[2]
+        hours = get_time_span(span)
+      else
+        raise "ups: #{time.inspect}"
       end
-    else
-      time += hours_s.to_f
+      
+      if hours.nil?
+        raise "oups, hours is nil?"
+      end
+      
+      if comment.match(/:/)
+        raise "colon in comment: #{comment.inspect}"
+      end
+      #puts " - #{comment} = #{hours}"
+      today += hours
     end
+    
+    puts today
+ 
   rescue => e
     puts line
     raise e
   end
   
-  stats_week[week] += time
-  entries << [week, date, time, desc]
+  stats_week[week] += today
+  entries << [week, week.to_s + " " +   weekday_s, today, comment]
   puts line
 end
 
