5bf52813f4b67d32b0d0eb0e43d5b19194aa9033
Robin Stocker <robin@nibor.org>
2008-06-05 20:56:45 +0200

    Fixed build.

--- tests/ch/hsr/ifs/pystructure/tests/playground/fibonacci/FibonacciTest.java
+++ tests/ch/hsr/ifs/pystructure/tests/playground/fibonacci/FibonacciTest.java
@@ -8,26 +8,26 @@ public class FibonacciTest extends TestCase {
 	public void testFibbonacci() {
 		Fibonacci fib = new Fibonacci();
 		
-		assertEquals(0, fib.calc(0));
-		assertEquals(1, fib.calc(1));
-		assertEquals(1, fib.calc(2));
-		assertEquals(2, fib.calc(3));
-		assertEquals(3, fib.calc(4));
-		assertEquals(5, fib.calc(5));
-		assertEquals(8, fib.calc(6));
-		assertEquals(13, fib.calc(7));
-		assertEquals(21, fib.calc(8));
-		assertEquals(34, fib.calc(9));
-		assertEquals(55, fib.calc(10));
-		assertEquals(89, fib.calc(11));
-		assertEquals(144, fib.calc(12));
-		assertEquals(233, fib.calc(13));
-		assertEquals(377, fib.calc(14));
-		assertEquals(610, fib.calc(15));
-		assertEquals(987, fib.calc(16));
-		assertEquals(1597, fib.calc(17));
-		assertEquals(2584, fib.calc(18));
-		assertEquals(4181, fib.calc(19));
+		assertEquals(0, fib.calculate(0));
+		assertEquals(1, fib.calculate(1));
+		assertEquals(1, fib.calculate(2));
+		assertEquals(2, fib.calculate(3));
+		assertEquals(3, fib.calculate(4));
+		assertEquals(5, fib.calculate(5));
+		assertEquals(8, fib.calculate(6));
+		assertEquals(13, fib.calculate(7));
+		assertEquals(21, fib.calculate(8));
+		assertEquals(34, fib.calculate(9));
+		assertEquals(55, fib.calculate(10));
+		assertEquals(89, fib.calculate(11));
+		assertEquals(144, fib.calculate(12));
+		assertEquals(233, fib.calculate(13));
+		assertEquals(377, fib.calculate(14));
+		assertEquals(610, fib.calculate(15));
+		assertEquals(987, fib.calculate(16));
+		assertEquals(1597, fib.calculate(17));
+		assertEquals(2584, fib.calculate(18));
+		assertEquals(4181, fib.calculate(19));
 		
 		fib.shutdown();	
 	}
